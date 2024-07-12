67c37872f2dbc75ae977885eeeedbcb2fd6e79e4
Aseem Raghav <aseem.raghav@innovaccer.com>
2024-07-12 16:43:36 +0530

    Add support for llama3-8b and llama3-70b

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from src.crew import MyCrew
 load_dotenv()
 
 if __name__ == "__main__":
-    crew = MyCrew()
+    crew = MyCrew(
+        llm_name="llama3_70b",
+        temperature=0.75,
+    )
     inputs = {
         "research_topic": """ "Environmental Life Cycle Assessment (LCA) of Bioactives (such as fucoidan, alginate, etc.) from seaweed (especially brown seaweed) at laboratory scale and pilot scale" """,
         "research_paper_section": "Introduction",
--- src/crew.py
+++ src/crew.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from crewai.process import Process
 from crewai import Crew
 from langchain_openai import AzureChatOpenAI
+from langchain_groq import ChatGroq
 from src.agents import MyAgents
 from src.tasks import MyTasks
 
@@ -11,21 +12,46 @@ load_dotenv()
 
 
 class MyCrew:
-    def __init__(self) -> None:
-        self.llm = self._create_llm()
+    def __init__(
+        self,
+        llm_name: str = "azure_openai",
+        temperature: float = 0.75,
+    ) -> None:
+        self.llm = self._create_llm(llm_name=llm_name, temperature=temperature)
         self.agents = self._create_agents()
         self.tasks = self._create_tasks()
         self.crew = self._create_crew()
 
-    def _create_llm(self) -> AzureChatOpenAI:
-        return AzureChatOpenAI(
-            openai_api_type="azure",
-            azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
-            openai_api_version=os.getenv("AZURE_OPENAI_VERSION"),
-            api_key=os.getenv("OPENAI_API_KEY"),
-            azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
-            temperature=0.75,
-        )
+    def _create_llm(
+        self,
+        llm_name: str,
+        temperature: float,
+    ) -> AzureChatOpenAI:
+        if llm_name == "azure_openai":
+            llm = AzureChatOpenAI(
+                openai_api_type="azure",
+                azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+                openai_api_version=os.getenv("AZURE_OPENAI_VERSION"),
+                api_key=os.getenv("OPENAI_API_KEY"),
+                azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
+                temperature=temperature,
+            )
+        elif llm_name == "llama3_8b":
+            llm = ChatGroq(
+                model="llama3-8b-8192",
+                api_key=os.getenv("GROQ_API_KEY"),
+                temperature=temperature,
+            )
+        elif llm_name == "llama3_70b":
+            llm = ChatGroq(
+                model="llama3-70b-8192",
+                api_key=os.getenv("GROQ_API_KEY"),
+                temperature=temperature,
+            )
+        else:
+            msg = f"`{llm_name}` LLM not supported yet."
+            raise NotImplementedError(msg)
+        return llm
 
     def _create_agents(self) -> MyAgents:
         return MyAgents(self.llm)
